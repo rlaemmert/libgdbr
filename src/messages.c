3225ea77383bbb87f4e831d42a5a44694026c6f4
defragger <rlaemmert@gmail.com>
2013-10-21 19:52:47 +0200

    bugfixing in reading g packet

--- src/messages.c
+++ src/messages.c
@@ -9,18 +9,10 @@ int handle_g(libgdbc_t* instance) {
 	int len = msg->len;
 	int i = 0;
 	printf("Len: %i\n", len);
-	while(len >= 0) {
-		printf("reg[%i] %s = ", i,x86_64[i].name);
+	while(len > 0) {
+		printf("reg[%i] %s = ", i, x86_64[i].name);
 		printf("%016llx\n", unpack_uint64_co ((msg->msg + x86_64[i].offset * 2), x86_64[i].size * 2));
 		len -= x86_64[i++].size * 2;
 	}
-
-//	for (i = 0;i<msg->len;i = i + 16) {
-//		uint8_t current[17];
-//		memcpy(current, msg->msg + i, 16);
-//		current[16] = '\0';
-//		uint64_t current_val = unpack_uint64_co (current, 16);
-//		printf("0x%016llx\n", current_val);
-//	}	
 	return 0;
 }

2760d0e9cce279beb13ad24d6e2e693317daff31
defragger <rlaemmert@gmail.com>
2014-02-16 10:25:57 +0100

    removed debug printf

--- src/messages.c
+++ src/messages.c
@@ -16,15 +16,12 @@ int handle_g(libgdbr_t* instance) {
 	//}
 	unpack_hex(instance->data, instance->data_len, instance->data);
 	instance->data_len = instance->data_len / 2;
-	printf("size: %i\n", instance->data_len);
-	hexdump(instance->data, instance->data_len, 0);
 	send_ack(instance);
 	return 0;
 }
 
 
 int handle_G(libgdbr_t* instance) {
-	printf("%s\n", instance->data);
 	send_ack(instance);
 	return 0;
 }
@@ -34,7 +31,6 @@ int handle_m(libgdbr_t* instance) {
 	int len = strlen(instance->data);
 	instance->data_len = strlen(instance->data) / 2;
 	unpack_hex(instance->data, len, instance->data);
-	//hexdump(instance->data, instance->data_len, 0);
 	send_ack(instance);
 	return 0;
 }
@@ -43,14 +39,13 @@ int handle_m(libgdbr_t* instance) {
 int handle_cmd(libgdbr_t* instance) {
 	unpack_hex(instance->data, strlen(instance->data), instance->data);
 	instance->data_len = strlen(instance->data) / 2;
-	printf("Answer: %s\n", instance->data); 
 	send_ack(instance);
 	return 0;
 }
 
 
 int handle_connect(libgdbr_t* instance) {
-	//printf("%s\n", instance->read_buff); // TODO handle the message correct and set all infos
+	// TODO handle the message correct and set all infos like packetsize, thread stuff and features
 	send_ack(instance);
 	return 0;
 }
